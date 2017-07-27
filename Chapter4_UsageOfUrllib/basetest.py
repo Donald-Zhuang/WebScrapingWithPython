aae0377ff26940d2ed92a407939a38a8f540de4d
donald.zhuang <1034894966@qq.com>
2017-07-27 09:19:42 -0700

    Update test on today

--- Chapter4_UsageOfUrllib/basetest.py
+++ Chapter4_UsageOfUrllib/basetest.py
@@ -3,17 +3,31 @@
 __author__ = 'donald.zhuang'
 import urllib.request
 
-file = urllib.request.urlopen('http://www.baidu.com')
-#print(file)
+'''[TEST] for urlopen'''
+Content = urllib.request.urlopen('http://www.baidu.com')
+# print(Content)
 
-data = file.read()
-print(data)
+'''[TEST] for read and readline'''
+data = Content.read()
+# print(data)
+#dataline = Content.readline()
+#print(dataline)
 
+'''[TEST] for file operation'''
 with open('baidu.html','wb') as hBaiduDoc:
     hBaiduDoc.write(data)
 
-#dataline = file.readline()
-#print(dataline)
+filename = urllib.request.urlretrieve("http://www.baidu.com", filename='2.html')
+urllib.request.urlcleanup()
+
+'''[TEST] other information'''
+print(Content.info())
+
+'''[TEST] some funtions'''
+print(Content.getcode()) # get the status code from http header
+print(Content.geturl())
+encodeUrl = print(urllib.request.quote(Content.geturl()))
+print(urllib.request.unquote('http%3A//www.baidu.com'))
 
 #if __name__ == '__main__':
 
