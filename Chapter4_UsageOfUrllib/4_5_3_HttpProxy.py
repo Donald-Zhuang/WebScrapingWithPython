3f9134cd2b45f8e99a71bf72c0ddf789a09fc310
donald.zhuang <1034894966@qq.com>
2017-08-01 08:04:57 -0700

    Test of DebugLog

--- Chapter4_UsageOfUrllib/4_5_3_HttpProxy.py
+++ Chapter4_UsageOfUrllib/4_5_3_HttpProxy.py
@@ -14,26 +14,32 @@ UrlToVisit  = 'http://www.baidu.com'
 def use_proxy(proxy_addr, url):
     '''build a proxy handler'''
     data = None
+    
     proxy   = urllib.request.ProxyHandler({'http':proxy_addr})
-    opener  = urllib.request.build_opener(proxy, urllib.request.HTTPHandler)
+    httphd  = urllib.request.HTTPHandler(debuglevel=1)
+    httpshd = urllib.request.HTTPSHandler(debuglevel=1)
+    
+    opener  = urllib.request.build_opener(proxy, httphd, httpshd)
     urllib.request.install_opener(opener)
+    
     try:
         data    = urllib.request.urlopen(url).read().decode('utf-8')
     except socket.error as SocketError:
         if SocketError.errno == errno.ECONNRESET:
-            print('Connection reset by peer')
+            print('\r\n[USER Test]Connection reset by peer')
         else:
             pass
+    
     return data
 
 if __name__ == '__main__':
     data = use_proxy(ProxyAddr, UrlToVisit)
     if data != None:
-        print(len(data))
+        print("\r\n[USER Test] the length of data : %d." % (len(data)) )
         '''write out data for test'''
         with open('test.html','w') as hfile:
             hfile.write(data)
     else:
-        print("Get Nothing")
+        print("\r\n[USER Test]Get Nothing")
 
 
