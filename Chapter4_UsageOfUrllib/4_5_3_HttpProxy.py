aa4c25859a3d22b0cc6efced0328db42bb30416b
donald.zhuang <1034894966@qq.com>
2017-08-01 07:54:13 -0700

    fix the bug that without using proxy and improve the fault tolerant

--- Chapter4_UsageOfUrllib/4_5_3_HttpProxy.py
+++ Chapter4_UsageOfUrllib/4_5_3_HttpProxy.py
@@ -4,22 +4,36 @@ __author__ = 'donald.zhuang'
 
 '''import the libraries'''
 import urllib.request
+import socket 
+import errno
 
 '''Global Variables'''
-ProxyAddr   = '111.155.116.221:8123'
+ProxyAddr   = '221.225.159.69:8118'
 UrlToVisit  = 'http://www.baidu.com'
 
 def use_proxy(proxy_addr, url):
     '''build a proxy handler'''
+    data = None
     proxy   = urllib.request.ProxyHandler({'http':proxy_addr})
     opener  = urllib.request.build_opener(proxy, urllib.request.HTTPHandler)
-    data    = urllib.request.urlopen(url).read().decode('utf-8')
+    urllib.request.install_opener(opener)
+    try:
+        data    = urllib.request.urlopen(url).read().decode('utf-8')
+    except socket.error as SocketError:
+        if SocketError.errno == errno.ECONNRESET:
+            print('Connection reset by peer')
+        else:
+            pass
     return data
 
 if __name__ == '__main__':
     data = use_proxy(ProxyAddr, UrlToVisit)
-    print(len(data))
-    '''write out data for test'''
-    with open('test.html','w') as hfile:
-        hfile.write(data)
+    if data != None:
+        print(len(data))
+        '''write out data for test'''
+        with open('test.html','w') as hfile:
+            hfile.write(data)
+    else:
+        print("Get Nothing")
+
 
