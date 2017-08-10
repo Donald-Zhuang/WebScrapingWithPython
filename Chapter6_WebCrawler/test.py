e65174b50623718d931cb7abdb9c60e6a8fd02e3
donald.zhuang <1034894966@qq.com>
2017-08-10 08:34:33 -0700

    test of MultiThread

--- Chapter6_WebCrawler/test.py
+++ Chapter6_WebCrawler/test.py
@@ -2,16 +2,24 @@
 #-*- coding: utf-8 -*-
 __author__ = 'donald.zhuang'
 
-import urllib.request
-import re
+import threading
 
-url = 'https://list.jd.com/list.html?cat=9987,653,655&page=2'
+class A(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        for i in range(0,1100):
+            print("A : %d" % i )
 
-html = urllib.request.urlopen(url).read().decode('utf-8')
+class B(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        for i in range(0,1100):
+            print("B : %d" % i )
 
-pat = '<img width="220" height="220" data-img="1" data-lazy-img="//(.+?(\.jpg|\.png){1,})">' # '<img width="220" height="220" data-img="1" data-lazy-img="//(.+?\.jpg)?(\>)?' 
-imagelist = re.compile(pat).findall(html)
-imagelist = set(imagelist)
-for urlphoto in imagelist:
-    print(urlphoto)
+t1 = A()
+t1.start()
+t2 = B()
+t2.start()
 
