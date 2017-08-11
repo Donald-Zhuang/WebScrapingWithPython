e14268dc64bb8ee76df3a0a530d0873cdf437d62
donald.zhuang <1034894966@qq.com>
2017-08-11 08:50:53 -0700

    demo for crawling articles on wechat

--- Chapter6_WebCrawler/6_4_WeChatCrawler.py
+++ Chapter6_WebCrawler/6_4_WeChatCrawler.py
@@ -17,39 +17,57 @@ Headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
         }
 
-def Build_CookieAndProxy(CookieFile=None):
+def Build_CookieAndProxy(CookieFile=None, ProxyAddr=None):
     '''build the cookie processor and install it for global use'''
+    #for Cookie
     cookie          = http.cookiejar.MozillaCookieJar(CookieFile)
     CookieProcessor = urllib.request.HTTPCookieProcessor(cookie)
-    CookieOpener    = urllib.request.build_opener(CookieProcessor,  urllib.request.HTTPHandler)
-    urllib.request.install_opener(CookieOpener)
-       
+    
+    #TODO: for Proxy
+    pass
+
+    Opener    = urllib.request.build_opener(CookieProcessor) 
+    urllib.request.install_opener(Opener)
+
+def Generate_PageUrl():
+    pass
+
 def Find_UrlAndTitleOnPage(PageUrl):
     
     setPhotoUrl = {}
     req     = urllib.request.Request(PageUrl, None, Headers)
     content = urllib.request.urlopen(req)
     data    = content.read().decode('utf-8')
-    with open('qiushi.html','w') as hFile:
-        hFile.write(data)
     setUrl = re.compile(RegExp_UrlTitle,re.S).findall(data)
     # setUrl = list(set(setUrl))
     return setUrl
 
-def Fix_UrlAndTitle(UrlTitle):
-    pass
+def Fix_UrlAndTitle(Url, Title):
+    UrlTitle = []
+    Url     = Url.replace( 'amp;', '' )
+    Title   = Title.replace( '<em><!--red_beg-->',  '' ).replace( '<!--red_end--></em>', '' )
+    UrlTitle.append(Url)
+    UrlTitle.append(Title)
+    return UrlTitle
 
 def Save_Article(Url,Title):
+    req = urllib.request.Request(Url, None, Headers)
+    data = urllib.request.urlopen(req).read().decode('utf-8')
+    with open(Title+'.html', 'w') as hFile:  # TODO: title name may cause some fault 
+        hFile.write(data)
+
+def Save_Photo(TitleUrl):
     pass
 
 def Main_Func():
     Url = UrlTemp % (KeyWd_Query, KeyWd_Type, KeyWd_Page)
     setText = Find_UrlAndTitleOnPage(Url)      # Get the set of Text
     for i in setText:
-        print(i)
+        i = Fix_UrlAndTitle(i[0], i[1])
+        Save_Article(i[0], i[1])
 
 if __name__ == '__main__':
-    Build_CookieAndProxy()
+    Build_Cookie()
     Main_Func()
 
 
